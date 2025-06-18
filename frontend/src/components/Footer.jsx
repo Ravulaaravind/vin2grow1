5a8a00b11dbddb8e8f165cb0be96369c1fd4ba6d
Ravulaaravind <130533726+Ravulaaravind@users.noreply.github.com>
2025-06-18 15:43:32 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -63,13 +63,13 @@ const Footer = () => {
                 <svg className="w-4 h-4 md:w-5 md:h-5 text-green-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <span className="text-gray-300 text-xs md:text-base">+91 9701555435</span>
+                <span className="text-gray-300 text-xs md:text-base">+917207711919</span>
               </li>
               <li className="flex items-center justify-center space-x-2 md:space-x-3">
                 <svg className="w-4 h-4 md:w-5 md:h-5 text-green-400 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                <span className="text-gray-300 text-xs md:text-base">support@vin2grow.com</span>
+                <span className="text-gray-300 text-xs md:text-base">svintogrow2@gmail.com</span>
               </li>
             </ul>
           </div>
